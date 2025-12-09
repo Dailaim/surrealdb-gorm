fe73bba546e718db2c438483d79ba5d343c50cd7
Leonardo Iglesias <laiglesias.min@gmail.com>
2025-12-08 21:21:35 -0500

    feat: add graph relations test

--- test/graph_test.go
+++ test/graph_test.go
@@ -2,86 +2,60 @@ package surrealdb_test
 
 import (
 	"testing"
-	"time"
 
-	"github.com/dailaim/surrealdb-gorm"
-	"github.com/dailaim/surrealdb-gorm/types"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
+	"github.com/dailaim/surrealdb-gorm/models"
 )
 
-type Student struct {
-	ID        *types.RecordID `gorm:"type:record;primaryKey"`
-	Name      string
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	Classes   []Class `gorm:"many2many:enrolled_in"`
+type Buyer struct {
+	models.Schemaless
+	Name string
 }
 
-type Class struct {
-	ID        *types.RecordID `gorm:"type:record;primaryKey"`
-	Title     string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+type Wishlist struct {
+	models.RelationSchemaless
 }
 
-func setupGraphDB(t *testing.T) *gorm.DB {
-	dsn := "wss://shiny-ocean-06dfnpnrpprgdf3fmb8e7vtpuo.aws-use1.surreal.cloud/rpc?namespace=test&database=test&username=root&password=root"
-	db, err := gorm.Open(surrealdb.Open(dsn), &gorm.Config{
-		SkipDefaultTransaction: true,
-		Logger:                 logger.Default.LogMode(logger.Info),
-	})
-	if err != nil {
-		t.Fatalf("Failed to connect: %v", err)
-	}
-
-	// Migrate models
-	err = db.AutoMigrate(&Student{}, &Class{})
-	if err != nil {
-		t.Fatalf("Failed to migrate: %v", err)
-	}
-	return db
+type Product struct {
+	models.Schemaless
+	Name string
 }
 
 func TestGraphManyToMany(t *testing.T) {
-	t.Skip("Graph support (RELATE, -> arrows) not yet implemented in GORM dialect")
-	db := setupGraphDB(t)
+	db := setupDB(t)
 
-	// Setup Schema cleanup (Raw SQL)
-	db.Exec("DELETE student")
-	db.Exec("DELETE class")
-	db.Exec("DELETE enrolled_in")
+	db.AutoMigrate(&Buyer{}, &Wishlist{}, &Product{})
 
-	student := Student{
+	buyer := Buyer{
 		Name: "Alice",
 	}
-	class := Class{
-		Title: "Math 101",
+	// cart := Cart{}
+	product := Product{
+		Name: "Product 1",
 	}
 
-	// 1. Create Student and Class
-	if err := db.Create(&student).Error; err != nil {
-		t.Fatalf("Failed to create student: %v", err)
+	// 1. Create Buyer and Class
+	if err := db.Create(&buyer).Error; err != nil {
+		t.Fatalf("Failed to create buyer: %v", err)
 	}
-	if err := db.Create(&class).Error; err != nil {
-		t.Fatalf("Failed to create class: %v", err)
-	}
-
-	t.Logf("Created Student: %v, Class: %v", student.ID, class.ID)
-
-	// 2. Create Association
-	// Default GORM will try to insert into `enrolled_in` (student_id, class_id)
-	err := db.Debug().Model(&student).Association("Classes").Append(&class)
-	if err != nil {
-		t.Logf("Association Append failed/warned: %v", err)
+	if err := db.Create(&product).Error; err != nil {
+		t.Fatalf("Failed to create product: %v", err)
 	}
 
-	// 3. Preload
-	var loadedStudent Student
-	err = db.Debug().Preload("Classes").First(&loadedStudent, "id = ?", student.ID).Error
-	if err != nil {
-		t.Logf("Preload failed: %v", err)
-	}
+	// t.Logf("Created Student: %v, Product: %v", student.ID, product.ID)
 
-	t.Logf("Loaded classes: %d", len(loadedStudent.Classes))
+	// // 2. Create Association
+	// // Default GORM will try to insert into `enrolled_in` (student_id, class_id)
+	// err := db.Debug().Model(&student).Association("Classes").Append(&class)
+	// if err != nil {
+	// 	t.Logf("Association Append failed/warned: %v", err)
+	// }
+
+	// // 3. Preload
+	// var loadedStudent Student
+	// err = db.Debug().Preload("Classes").First(&loadedStudent, "id = ?", student.ID).Error
+	// if err != nil {
+	// 	t.Logf("Preload failed: %v", err)
+	// }
+
+	// t.Logf("Loaded classes: %d", len(loadedStudent.Classes))
 }
